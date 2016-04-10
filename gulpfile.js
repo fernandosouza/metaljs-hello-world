546ed288b0e8cb61555a7548cd28576400e38353
Fernando Souza <nandosouzafilho@gmail.com>
2016-04-09 22:16:19 -0300

    Just after run yo metal

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'helloWorld.css',
+	bundleFileName: 'helloWorld.js',
+	moduleName: 'metal-helloWorld'
+});
---
+++ test/HelloWorld.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import HelloWorld from '../src/HelloWorld';
+
+describe('HelloWorld', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
